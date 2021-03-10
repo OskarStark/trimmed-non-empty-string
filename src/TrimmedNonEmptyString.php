db46ef99c52448b271ed010a6fe164c4e7efbf11
Oskar Stark <oskarstark@googlemail.com>
2021-03-10 13:39:53 +0100

    Fix: Cs

--- src/TrimmedNonEmptyString.php
+++ src/TrimmedNonEmptyString.php
@@ -2,10 +2,19 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of oskarstark/trimmed-non-empty-string.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace OskarStark\Value;
 
-use Webmozart\Assert\Assert;
 use function Symfony\Component\String\u;
+use Webmozart\Assert\Assert;
 
 final class TrimmedNonEmptyString
 {
--- tests/TrimmedNonEmptyStringTest.php
+++ tests/TrimmedNonEmptyStringTest.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of oskarstark/trimmed-non-empty-string.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace OskarStark\Value\Tests;
 
 use Ergebnis\Test\Util\Helper;
@@ -21,7 +30,7 @@ final class TrimmedNonEmptyStringTest extends TestCase
 
         $string = TrimmedNonEmptyString::fromString($value);
 
-        self::assertSame($value, $string->toString());
+        static::assertSame($value, $string->toString());
     }
 
     /**
